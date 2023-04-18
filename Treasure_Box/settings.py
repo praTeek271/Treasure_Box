8a26f0f0248ed85b0ea449e72fe5821378d6763f
Prateek Kumar Singh <66623022+praTeek271@users.noreply.github.com>
2023-04-18 23:02:21 +0530

    Update settings.py

--- Treasure_Box/settings.py
+++ Treasure_Box/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3%3(*i6hmm-j#j8i#$*=#d%i=^rprfj%3v6%ns(#n!d+#*gs!5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
