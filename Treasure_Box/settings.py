52c278406ea36f135726bea4f13fb1bcd2fef8e2
prateek271 <prateekpatriotasme@gmail.com>
2023-10-19 07:10:23 +0530

    patch complete

--- Treasure_Box/settings.py
+++ Treasure_Box/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-3%3(*i6hmm-j#j8i#$*=#d%i=^rprfj%3v6%ns(#n!d+#*gs!5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- Treasure_Box/views.py
+++ Treasure_Box/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 
 def index(request):
     # return HttpResponse("Hello, world. You're at the polls index.")
-    return(render(request,'homePage.html'))
+    return(render(request,'test.html'))
